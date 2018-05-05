a5e48a8980c6ead3aec3b42bec4fbbfba455b9cf
Harshit Sinha <harshitsinha1102@gmail.com>
2018-05-06 03:01:27 +0530

    Making headlines component

--- src/components/page-body/index.js
+++ src/components/page-body/index.js
@@ -1,25 +1,8 @@
 import React from "react";
-import NewsService from "../../services/news";
-
+import Headlines from "../headlines";
 export default class PageBody extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      headlines: '',
-      newsElements: [],
-      isLoading: true
-    };
-    this.newsService = new NewsService();
-  }
-
-  componentDidMount() {
-    this.newsService.fetchTopHeadlines()
-      .then(response => {
-        let news = response.data;
-        this.setState({ headlines: news });
-        this.setState({ newsElements: this.state.headlines.articles.map((article, index) => <li key={'news_' + index}>{article.title}</li>) });
-        this.setState({ isLoading: false });
-      });
   }
 
   render() {
@@ -27,11 +10,7 @@ export default class PageBody extends React.Component {
       <div className="container-fluid">
         <div className="row">
           <div className="col-lg-8">
-            <div className="container-fluid animationload">
-              <div className={this.state.isLoading ? "osahanloading" : ""}>
-                <ul>{this.state.newsElements}</ul>
-              </div>
-            </div>
+            <Headlines />
           </div>
           <div className="col-lg-4"></div>
         </div>
