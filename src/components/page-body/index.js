372310913be18cda1791f74ed706ae4a6313f742
Harshit Sinha <harshitsinha1102@gmail.com>
2018-05-06 02:47:04 +0530

    Adding loader when data not fetched

--- src/components/page-body/index.js
+++ src/components/page-body/index.js
@@ -4,15 +4,21 @@ import NewsService from "../../services/news";
 export default class PageBody extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { headlines: '', newsElements: [] };
+    this.state = {
+      headlines: '',
+      newsElements: [],
+      isLoading: true
+    };
     this.newsService = new NewsService();
   }
 
   componentDidMount() {
-    this.newsService.fetchHeadlines()
-      .then(news => {
+    this.newsService.fetchTopHeadlines()
+      .then(response => {
+        let news = response.data;
         this.setState({ headlines: news });
         this.setState({ newsElements: this.state.headlines.articles.map((article, index) => <li key={'news_' + index}>{article.title}</li>) });
+        this.setState({ isLoading: false })
       });
   }
 
@@ -21,7 +27,11 @@ export default class PageBody extends React.Component {
       <div className="container-fluid">
         <div className="row">
           <div className="col-lg-8">
-            <ul>{this.state.newsElements}</ul>
+            <div className="container-fluid animationload">
+              <div className={this.state.isLoading ? "osahanloading" : ""}>
+                <ul>{this.state.newsElements}</ul>
+              </div>
+            </div>
           </div>
           <div className="col-lg-4"></div>
         </div>
